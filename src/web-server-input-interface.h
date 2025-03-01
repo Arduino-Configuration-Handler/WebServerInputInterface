6e55d50de61166164311a3b80ef5dacdccff7db3
SagiZiv3 <73391391+SagiZiv3@users.noreply.github.com>
2025-03-01 16:28:58 +0200

    Fixed copy-paste mistake

--- src/web-server-input-interface.h
+++ src/web-server-input-interface.h
@@ -1,7 +1,7 @@
-#ifndef __H_PREFERENCES_STORAGE__
-#define __H_PREFERENCES_STORAGE__
+#ifndef __H_WEB_SERVER_INTERFACE__
+#define __H_WEB_SERVER_INTERFACE__
 
 #include <config-handler-core.h>
 #include "WebServerInputInterface.h"
 
-#endif  // __H_PREFERENCES_STORAGE__
+#endif  // __H_WEB_SERVER_INTERFACE__
